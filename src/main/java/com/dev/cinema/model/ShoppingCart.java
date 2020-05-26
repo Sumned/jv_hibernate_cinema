17f1e55832553e07a4dcef0fa19a1a1e7c88490b
Sumned <vladar91@gmail.com>
2020-05-26 21:59:47 +0300

    arraylist initialization added to model

--- src/main/java/com/dev/cinema/model/ShoppingCart.java
+++ src/main/java/com/dev/cinema/model/ShoppingCart.java
@@ -1,5 +1,6 @@
 package com.dev.cinema.model;
 
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -18,7 +19,7 @@ public class ShoppingCart {
     private Long id;
 
     @OneToMany(cascade = CascadeType.PERSIST)
-    private List<Ticket> tickets;
+    private List<Ticket> tickets = new ArrayList<>();
 
     @OneToOne(cascade = CascadeType.PERSIST)
     private User user;
